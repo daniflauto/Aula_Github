d5b3bb08a7bedb09e73bd0298c87f48cb10b001b
daniflauto <danibflauto@hotmail.com>
2023-06-29 16:42:01 -0300

    Initial Commit

---
+++ AulaGithub/AulaGithubApp.swift
@@ -0,0 +1,17 @@
+//
+//  AulaGithubApp.swift
+//  AulaGithub
+//
+//  Created by Daniela Brazolin Flauto on 29/06/23.
+//
+
+import SwiftUI
+
+@main
+struct AulaGithubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
